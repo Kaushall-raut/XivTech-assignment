5e8f952197ffbff8dafabf0c160cfb46df781eee
Kaushall-raut <KaushalRaut.code@gmail.com>
2025-04-24 21:07:13 +0530

    added sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import "../index.css";
+
+const Sidebar = () => {
+  return (
+    <aside className="sidebar">
+    <h2 className="sidebar-title">Crypto Tracker</h2>
+    <nav className="sidebar-nav">
+      <a href="#" className="sidebar-link">Home</a>
+      <a href="#" className="sidebar-link">about</a>
+      <a href="#" className="sidebar-link">Track Crypto</a>
+    </nav>
+  </aside>
+  )
+}
+
+export default Sidebar
