92ad529ac018d9ef9fa0244b198cd2fb9b270865
Kaushall-raut <KaushalRaut.code@gmail.com>
2025-04-25 19:21:53 +0530

    fixed some bug

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,5 @@
 const About = () => {
-  return <div><h1>About ME</h1>Hi, I&apos;m Kaushal , a passionate full-stack developer with a love for building scalable, user-friendly applications. With a strong foundation in both frontend and backend technologies, I enjoy working on projects that solve real-world problems. I'm constantly learning and exploring new tools and frameworks to stay updated with the latest industry trends. When I'm not coding, you can find me experimenting with new technologies, contributing to open-source projects, or sharing my knowledge with the developer community</div>;
+  return <div><h1>About ME</h1>Hi, I&apos;m Kaushal , a passionate full-stack developer with a love for building scalable, user-friendly applications. With a strong foundation in both frontend and backend technologies, I enjoy working on projects that solve real-world problems. I&apos;m constantly learning and exploring new tools and frameworks to stay updated with the latest industry trends. When I&apos;m not coding, you can find me experimenting with new technologies, contributing to open-source projects, or sharing my knowledge with the developer community</div>;
 };
 
 export default About;
